b2fd4f9c02a4fb6a0bbc1233c2a76d86c91bd5a3
Mechanic <gazizovrustam.r@gmail.com>
2022-05-27 16:52:42 +0300

    Final

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 def main():
     ticketA = int(input('Please enter tickets sold category A: '))
-    value = getTicketA(ticketA)
-    print('$', value)
+    ticketB = int(input('Please enter tickets sold category B: '))
+    ticketC = int(input('Please enter tickets sold category C: '))
+
+    priceA = 20
+    priceB = 15
+    priceC = 10
+
+    valueA = getTicket(ticketA, priceA)
+    valueB = getTicket(ticketB, priceB)
+    valueC = getTicket(ticketC, priceC)
+    totalSum = getTotalSum(valueA, valueB, valueC)
+    print('$', totalSum)
 
 
-def getTicketA(ticketA):
-    return ticketA * 20
+def getTicket(ticket, price):
+    return ticket * price
+
+def getTotalSum(value1, value2, value3):
+    return value1 + value2 + value3
 
 
 main()
